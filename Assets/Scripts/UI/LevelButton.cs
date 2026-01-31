9b9079fa659fb77b035fc01f0ad09dd1af5af04e
Akshay Jain <akshay1427@gmail.com>
2026-01-31 21:51:50 +0000

    Set up scriptable objects for three test levels

---
+++ Assets/Scripts/UI/LevelButton.cs
@@ -0,0 +1,35 @@
+using Core;
+using Core.MessageBroker;
+using Core.Services;
+using ScriptableObjects;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class LevelButton : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text buttonText;
+        private LevelDesignerScriptableObject _level;
+        private MessageBroker _messageBroker;
+        
+        private void Start()
+        {
+            if (ServiceLocator.Instance.TryGet(out MessageBroker messageBroker))
+            {
+                _messageBroker = messageBroker;
+            }
+        }
+
+        public void SetLevel(LevelDesignerScriptableObject level)
+        {
+            _level = level;
+            buttonText.text = level.LevelName;
+        }
+
+        public void SelectLevel()
+        {
+            _messageBroker.Publish(new StartGameEvent(_level));
+        }
+    }
+}
---
+++ Assets/Scripts/UI/LevelsManager.cs
@@ -0,0 +1,77 @@
+using Core;
+using MemoryMatch.DataModel;
+using UnityEngine;
+using Core.Config;
+using Core.MessageBroker;
+using Core.Services;
+using ScriptableObjects;
+using Scriptables;
+using UnityEngine.EventSystems;
+
+namespace UI
+{
+    public class LevelsManager : MonoBehaviour
+    {
+        [SerializeField] private LevelsScriptableObject levels;
+        [SerializeField] private Transform levelsContainer;
+        [SerializeField] private LevelButton levelButtonPrefab;
+        
+        private IFileService _fileService;
+        private MessageBroker _messageBroker;
+        
+        void Awake()
+        {
+            PopulateGrid();
+        }
+        
+        private void Start()
+        {
+            if (ServiceLocator.Instance.TryGet(out MessageBroker messageBroker))
+            {
+                _messageBroker = messageBroker;
+                _messageBroker.Subscribe<StartGameEvent>(StartGame);
+            }
+            
+            if (ServiceLocator.Instance.TryGet(out FileService fileService))
+            {
+                _fileService = fileService;
+            }
+        }
+        
+        private void StartGame(StartGameEvent startGameEvent)
+        {
+            LevelData level = new LevelData(startGameEvent.Level);
+            _fileService.WriteToFile(level, Constants.Filenames.CurrentLevel);
+        }
+        
+        private void PopulateGrid()
+        {
+            // Clear existing children
+            ClearGrid();
+
+            // Create new elements
+            foreach (LevelDesignerScriptableObject level in levels.Levels)
+            {
+                LevelButton cell = Instantiate(levelButtonPrefab, levelsContainer.transform);
+                cell.SetLevel(level);
+                cell.name = level.LevelName;
+            }
+        }
+        
+        private void ClearGrid()
+        {
+            for (int i = levelsContainer.transform.childCount - 1; i >= 0; i--)
+            {
+                if (Application.isPlaying)
+                {
+                    Destroy(levelsContainer.transform.GetChild(i).gameObject);
+                }
+                else
+                {
+                    DestroyImmediate(levelsContainer.transform.GetChild(i).gameObject);
+                }
+            }
+        }
+    }
+
+}
