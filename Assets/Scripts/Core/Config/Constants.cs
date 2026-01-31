721dc25e88f570e1f1f1a9f9ab22d70ed8e18a60
Akshay Jain <akshay1427@gmail.com>
2026-01-31 20:31:37 +0000

    Add main menu and game scenes

---
+++ Assets/Scripts/Core/Config/Constants.cs
@@ -0,0 +1,11 @@
+namespace Core.Config
+{
+    public static class Constants
+    {
+        public static class Scenes
+        {
+            public const string MainMenu = "MainMenuScene";
+            public const string GameScene = "GameScene";
+        }
+    }
+}
